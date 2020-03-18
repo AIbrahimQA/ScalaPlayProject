c8dbdc35fd69cfb95544559a67ad64b902ebbc1e
ahmed <ahmed.ibrahim@academytrainee.com>
2020-03-18 10:52:46 +0000

    Added Song submit

---
+++ app/controllers/SongController.scala
@@ -0,0 +1,23 @@
+package controllers
+import javax.inject.{Inject, Singleton}
+import javax.inject._
+import models.SongInfo
+import play.api.mvc._
+
+
+@Singleton
+class SongController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
+
+  def showSongForm() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.submitSong(SongInfo.postSong))
+  }
+
+  def submitSong = Action { implicit request: Request[AnyContent] =>
+    SongInfo.postSong.bindFromRequest.fold({ formWithErrors =>
+      BadRequest(views.html.submitSong(formWithErrors))
+    }, { cd =>
+      Ok(cd.toString())
+    })
+  }
+
+}
